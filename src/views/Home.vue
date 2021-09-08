676a1d5b3b5fbfe17d91b1e467eafc34bef8cfe3
Alena Savl <damkina.ks@gmail.com>
2021-09-08 16:16:33 +0300

    git commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="white">
+    <vHeader msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import vHeader from "@/components/vHeader.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    vHeader,
+  },
+};
 </script>
